2f88160b214a3139c036541f35848354f188144c
Sidra Khan <sidrakhan.me.100@gmail.com>
2023-11-22 17:53:27 +0500

    📦NEW: Initial Work

---
+++ src/components/Player.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [playerName, setPlayerName] = useState(name);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <>
+      <li>
+        <span className="player">
+          {isEditing ? (
+            <input
+              type="text"
+              required
+              value={playerName}
+              onChange={handleChange}
+            />
+          ) : (
+            <span className="player-name">{playerName}</span>
+          )}
+          <span className="player-symbol">{symbol}</span>
+        </span>
+        <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+      </li>
+    </>
+  );
+}
